7696ef001042f76f4f35b3f881dad91657e2a0b5
Bruno Dhein <dhein.brunoruan@gmail.com>
2019-10-21 18:49:54 -0300

    Módulo 1

---
+++ Modulo-1/Exercício 1/exercício1.js
@@ -0,0 +1,20 @@
+
+      var endereco = {
+        rua: "Rua dos pinheiros",
+        numero: 1293,
+        bairro: "centro",
+        cidade: "São Paulo",
+        uf: "SP"
+      };
+      console.log(
+        "O usuário mora em",
+        endereco.cidade,
+        "/",
+        endereco.uf,
+        ",no bairro Centro,",
+        "na rua",
+        endereco.rua,
+        "com nº",
+        endereco.numero
+      );
+   
---
+++ Modulo-1/Exercício 2/exercício2.js
@@ -0,0 +1,11 @@
+
+      function pares(x, y) {
+        var x;
+        for (x; x <= y; x++) {
+          if (x % 2 === 0) {
+            console.log(x);
+          }
+        }
+      }
+      pares(5, 20);
+   
